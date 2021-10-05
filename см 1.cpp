2a47cd4c64129e1e407b7dfbd147e841c3cd6906
matveysurzhansky <91685858+matveysurzhansky@users.noreply.github.com>
2021-10-05 17:07:52 +0300

    Add files via upload

---
+++ см 1.cpp
@@ -0,0 +1,16 @@
+// матвей суржанский п43021 
+//Написать программу вычесления стоимости некоторого количества яблок по весу.
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	int b;
+	int c;
+	cout << "цена за один кило яблок";
+	cin >> a;
+	cout << "сколько кило берешь ?";
+	cin >> b;
+	c = a * b;
+	cout << "цена за покупку " << c;
+}
